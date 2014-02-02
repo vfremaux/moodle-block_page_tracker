fd073bf58ce4afe332ffd8d475deef63f5086237
Valery Fremaux <valery.fremaux@gmail.com>
2014-02-03 00:12:25 +0100

    fix dependencies

--- version.php
+++ version.php
@@ -32,3 +32,4 @@ $plugin->requires  = 2012120300;        // Requires this Moodle version
 $plugin->component = 'block_page_tracker'; // Full name of the plugin (used for diagnostics)
 $plugin->release = '2.4.0 (Build 2013031400)';
 $plugin->maturity = MATURITY_RC;
+$module->dependencies = array('format_page' => 2013012900);
