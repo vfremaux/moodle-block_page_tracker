2e60ce5a4edc961089fa2bad9c5c0e61c2f00a71
valery <valery.fremaux@gmail.com>
2014-06-19 15:22:44 +0200

    code core cleanup

--- block_page_tracker.php
+++ block_page_tracker.php
@@ -1,25 +1,44 @@
-<?php //$Id: block_page_tracker.php,v 1.5 2012-02-16 19:53:54 vf Exp $
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// generates a menu list of child pages ("stations") for a paged format course
+
+/*
+ * generates a menu list of child pages ("stations") for a paged format course
+ */
 
 require_once($CFG->dirroot.'/course/format/page/lib.php');
 
 class block_page_tracker extends block_list {
 
-    function init() {
-        $this->title = get_string('blockname', 'block_page_tracker') ;
+    public function init() {
+        $this->title = get_string('blockname', 'block_page_tracker');
     }
 
-    function specialization() {
-        if (!empty($this->config) && !empty($this->config->title)) $this->title = format_string($this->config->title) ;
-    }
-    
-    function instance_allow_config(){
-    	return true;
+    public function specialization() {
+        if (!empty($this->config) && !empty($this->config->title)) {
+            $this->title = format_string($this->config->title);
+        }
     }
 
-    function instance_allow_multiple(){
-    	return true;
+    public function instance_allow_config() {
+        return true;
+    }
+
+    function instance_allow_multiple() {
+        return true;
     }
 
     function applicable_formats() {
@@ -31,8 +50,8 @@ class block_page_tracker extends block_list {
             return $this->content;
         }
         
-        if (!isset($this->config->depth)){
-        	@$this->config->depth = 100;
+        if (!isset($this->config->depth)) {
+            @$this->config->depth = 100;
         }
 
         $filteropt = new stdClass;
@@ -48,160 +67,166 @@ class block_page_tracker extends block_list {
     function generate_summary() {
         global $CFG, $USER, $COURSE, $DB, $OUTPUT;
 
-		if (!$courseid = $COURSE->id){
-	        $courseid = $this->instance->pageid;
-	    }
-	    
-	    if (!isset($this->config->startpage)) @$this->config->startpage = 0;
-	    
-        if ($this->config->startpage){
-        	$startpage = course_page::get($this->config->startpage, $COURSE->id);
-	        $pages = $startpage->get_children();
-	    } else {
-	    	$pages = course_page::get_all_pages($courseid, 'nested');
-	    }
-
-        $current = course_page::get_current_page($courseid);
-        
-        if (empty($pages)){
-        	return '';
+        if (!$courseid = $COURSE->id) {
+            $courseid = $this->instance->pageid;
         }
 
-		// Resolve tickimage locations
-		$ticks = new StdClass;
-		$ticks->image = $OUTPUT->pix_url('tick_green_big', 'block_page_tracker');
-		$ticks->imagepartial = $OUTPUT->pix_url('tick_green_big_partial', 'block_page_tracker');
-		$ticks->imageempty = $OUTPUT->pix_url('tick_green_big_empty', 'block_page_tracker');
+        if (!isset($this->config->startpage)) {
+            @$this->config->startpage = 0;
+        }
+
+        if ($this->config->startpage) {
+            $startpage = course_page::get($this->config->startpage, $COURSE->id);
+            $pages = $startpage->get_children();
+        } else {
+            $pages = course_page::get_all_pages($courseid, 'nested');
+        }
+
+        $current = course_page::get_current_page($courseid);
+
+        if (empty($pages)) {
+            return '';
+        }
+
+        // Resolve tickimage locations.
+        $ticks = new StdClass;
+        $ticks->image = $OUTPUT->pix_url('tick_green_big', 'block_page_tracker');
+        $ticks->imagepartial = $OUTPUT->pix_url('tick_green_big_partial', 'block_page_tracker');
+        $ticks->imageempty = $OUTPUT->pix_url('tick_green_big_empty', 'block_page_tracker');
         
         $this->content->items = array();
         $this->content->icons = array();
 
-        // todo: if in my learning paths check completion for tick display 
+        // TODO : if in my learning paths check completion for tick display.
 
-		// pre scans page for completion compilation
+        // Pre scans page for completion compilation.
         foreach ($pages as $page) {
-        	$page->accessed = $DB->record_exists_select('log', " userid = ? AND course = ? AND action = 'viewpage' AND info = ? ", array($USER->id, $courseid, "{$courseid}:{$page->id}"));
-        	if ($page->has_children()){
-	        	$page->complete = $page->accessed && $this->check_childs_access($page);
-	        } else {
-	        	$page->complete = $page->accessed;
-	        }
+            $page->accessed = $DB->record_exists_select('log', " userid = ? AND course = ? AND action = 'viewpage' AND info = ? ", array($USER->id, $courseid, "{$courseid}:{$page->id}"));
+            if ($page->has_children()) {
+                $page->complete = $page->accessed && $this->check_childs_access($page);
+            } else {
+                $page->complete = $page->accessed;
+            }
         }
 
         foreach ($pages as $page) {
-        	if (!$page->is_visible(true)) continue;
-        	$realvisible = $page->is_visible(false);
-    		$class = ($realvisible) ? '' : 'shadow ';
-        	$class .= ($current->id == $page->id) ? 'current' : '' ;
+            if (!$page->is_visible(true)) {
+                continue;
+            }
+            $realvisible = $page->is_visible(false);
+            $class = ($realvisible) ? '' : 'shadow ';
+            $class .= ($current->id == $page->id) ? 'current' : '';
             $isenabled = $page->check_activity_lock();
-            if ($page->accessed){
-            	if ($page->complete){
-            		$image = $ticks->image; 
-            	} else {
-            		$image = $ticks->imagepartial;
-            	}
+            if ($page->accessed) {
+                if ($page->complete) {
+                    $image = $ticks->image; 
+                } else {
+                    $image = $ticks->imagepartial;
+                }
             } else {
-            	$image = $ticks->imageempty;
+                $image = $ticks->imageempty;
             }
             
-            if (!empty($this->config->usemenulabels)){
-            	$pagename = format_string($page->nametwo);
-            	if (empty($pagename)){
-            		$pagename = format_string($page->nameone);
-            	}
+            if (!empty($this->config->usemenulabels)) {
+                $pagename = format_string($page->nametwo);
+                if (empty($pagename)) {
+                    $pagename = format_string($page->nameone);
+                }
             } else {
-            	$pagename = format_string($page->nameone);
+                $pagename = format_string($page->nameone);
             }
 
-            if ((@$this->config->allowlinks == 2 || (@$this->config->allowlinks == 1 && $page->accessed)) && $isenabled){
-	            $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$page->get_page_depth().'"><a href="/course/view.php?id='.$courseid.'&amp;page='.$page->id.'" class="block-pagetracker '.$class.'">'.$pagename.'</a></div>';
-	            if (empty($this->config->hideaccessbullets)){
-		            $this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
-		        }
-	        } else {
-	        	if (empty($this->config->hidedisabledlinks)){
-		            $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$page->get_page_depth().'">'.$pagename.'</div>';
-		            if (empty($this->config->hideaccessbullets)){
-				    	$this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
-					}
-		        }
-	        }
-	        
-	        if ($page->has_children() && ($this->config->depth - 1 > 0)){
-		        $this->print_sub_stations($page, $ticks, $current, $this->config->depth - 2);
-		    }
+            if ((@$this->config->allowlinks == 2 || (@$this->config->allowlinks == 1 && $page->accessed)) && $isenabled) {
+                $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$page->get_page_depth().'"><a href="/course/view.php?id='.$courseid.'&amp;page='.$page->id.'" class="block-pagetracker '.$class.'">'.$pagename.'</a></div>';
+                if (empty($this->config->hideaccessbullets)) {
+                    $this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
+                }
+            } else {
+                if (empty($this->config->hidedisabledlinks)) {
+                    $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$page->get_page_depth().'">'.$pagename.'</div>';
+                    if (empty($this->config->hideaccessbullets)) {
+                        $this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
+                    }
+                }
+            }
+            
+            if ($page->has_children() && ($this->config->depth - 1 > 0)) {
+                $this->print_sub_stations($page, $ticks, $current, $this->config->depth - 2);
+            }
         }
 
         return $this->content;
-	}
+    }
 
    /**
     * Recursive down scann
     */
-    function check_childs_access(&$page){
-		global $USER, $COURSE, $DB;
-		
-		$complete = true;
-		$children = $page->get_children();
-    	foreach($children as &$child){
-        	$child->accessed = $DB->record_exists_select('log', " userid = ? AND course = ? AND action = 'viewpage' AND info = ? ", array($USER->id, $COURSE->id, "{$COURSE->id}:{$child->id}"));
-        	if ($child->has_children()){
-	        	$child->complete = $child->accessed && $this->check_childs_access($child);
-	        } else {
-	        	$child->complete = $child->accessed;
-	        }
-	        $complete = $complete && $child->accessed;
-    	}
-    	
-    	return $complete;
+    function check_childs_access(&$page) {
+        global $USER, $COURSE, $DB;
+
+        $complete = true;
+        $children = $page->get_children();
+        foreach ($children as &$child) {
+            $child->accessed = $DB->record_exists_select('log', " userid = ? AND course = ? AND action = 'viewpage' AND info = ? ", array($USER->id, $COURSE->id, "{$COURSE->id}:{$child->id}"));
+            if ($child->has_children()) {
+                $child->complete = $child->accessed && $this->check_childs_access($child);
+            } else {
+                $child->complete = $child->accessed;
+            }
+            $complete = $complete && $child->accessed;
+        }
+
+        return $complete;
     }
     
-    function print_sub_stations(&$page, &$ticks, $current, $currentdepth){
-    	global $CFG, $COURSE, $OUTPUT;
-    	
-    	$children = $page->get_children();
-    	foreach($children as &$child){
-    		if (!$child->is_visible(true)) continue;
-    		$realvisible = $child->is_visible(false);
-    		$class = ($realvisible) ? '' : 'shadow ';
-        	$class .= ($current->id == $child->id) ? 'current' : '' ;
+    function print_sub_stations(&$page, &$ticks, $current, $currentdepth) {
+        global $CFG, $COURSE, $OUTPUT;
+
+        $children = $page->get_children();
+        foreach($children as &$child) {
+            if (!$child->is_visible(true)) {
+                continue;
+            }
+            $realvisible = $child->is_visible(false);
+            $class = ($realvisible) ? '' : 'shadow ';
+            $class .= ($current->id == $child->id) ? 'current' : '';
             $isenabled = $child->check_activity_lock();
-            if (@$child->accessed){
-            	if ($child->complete){
-            		$image = $ticks->image;
-            	} else {
-            		$image = $ticks->imagepartial;
-            	}
+            if (@$child->accessed) {
+                if ($child->complete) {
+                    $image = $ticks->image;
+                } else {
+                    $image = $ticks->imagepartial;
+                }
             } else {
-            	$image = $ticks->imageempty;
+                $image = $ticks->imageempty;
             }
 
-            if (!empty($this->config->usemenulabels)){
-            	$childname = format_string($child->nametwo);
-            	if (empty($childname)){
-            		$childname = format_string($child->nameone);
-            	}
+            if (!empty($this->config->usemenulabels)) {
+                $childname = format_string($child->nametwo);
+                if (empty($childname)) {
+                    $childname = format_string($child->nameone);
+                }
             } else {
-            	$childname = format_string($child->nameone);
+                $childname = format_string($child->nameone);
             }
 
-            if ((@$this->config->allowlinks == 2 || (@$this->config->allowlinks == 1 && $child->accessed)) && $isenabled){
-	            $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$child->get_page_depth().'"><a href="/course/view.php?id='.$COURSE->id.'&amp;page='.$child->id.'" class="block-pagetracker '.$class.'">'.$childname.'</a></div>';
-	            if (empty($this->config->hideaccessbullets)){
-		            $this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
-		        }
-	        } else {
-	        	if (empty($this->config->hidedisabledlinks)){
-		            $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$child->get_page_depth().'">'.$childname.'</div>';
-	            	if (empty($this->config->hideaccessbullets)){
-		            	$this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
-		            }
-		        }
-	        }
-	        
-	        if ($child->has_children() && ($currentdepth > 0)){
-		        $this->print_sub_stations($child, $ticks, $current, $currentdepth - 1);
-		    }
-    	}
+            if ((@$this->config->allowlinks == 2 || (@$this->config->allowlinks == 1 && $child->accessed)) && $isenabled) {
+                $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$child->get_page_depth().'"><a href="/course/view.php?id='.$COURSE->id.'&amp;page='.$child->id.'" class="block-pagetracker '.$class.'">'.$childname.'</a></div>';
+                if (empty($this->config->hideaccessbullets)) {
+                    $this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
+                }
+            } else {
+                if (empty($this->config->hidedisabledlinks)) {
+                    $this->content->items[] = '<div class="block-pagetracker '.$class.' pagedepth'.@$child->get_page_depth().'">'.$childname.'</div>';
+                    if (empty($this->config->hideaccessbullets)) {
+                        $this->content->icons[] = '<img border="0" align="left" src="'.$image.'" width="15" />';
+                    }
+                }
+            }
+
+            if ($child->has_children() && ($currentdepth > 0)) {
+                $this->print_sub_stations($child, $ticks, $current, $currentdepth - 1);
+            }
+        }
     }
 }
--- edit_form.php
+++ edit_form.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -27,50 +26,49 @@ if (!defined('MOODLE_INTERNAL')) die ('You cannot use this script this way');
 
 class block_page_tracker_edit_form extends block_edit_form {
     protected function specific_definition($mform) {
-    	global $DB, $CFG, $COURSE;
-    	
+        global $DB, $CFG, $COURSE;
+
         // Fields for editing HTML block title and contents.
 
         $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
 
         $mform->addElement('text', 'config_title', get_string('configtitle', 'block_page_tracker'));
         $mform->setType('config_title', PARAM_MULTILANG);
-	
-    	$linkoptions['0'] = get_string('no');
-    	$linkoptions['1'] = get_string('yesonvisited', 'block_page_tracker');
-    	$linkoptions['2'] = get_string('yes');
-		$mform->addElement('select', 'config_allowlinks', get_string('allowlinks', 'block_page_tracker'), $linkoptions);
 
-		$mform->addElement('checkbox', 'config_hidedisabledlinks', get_string('hidedisabledlinks', 'block_page_tracker'));
-		$mform->setDefault('config_hidedisabledlinks', 0);
+        $linkoptions['0'] = get_string('no');
+        $linkoptions['1'] = get_string('yesonvisited', 'block_page_tracker');
+        $linkoptions['2'] = get_string('yes');
+        $mform->addElement('select', 'config_allowlinks', get_string('allowlinks', 'block_page_tracker'), $linkoptions);
 
-    	$pageoptions = array();
-    	$pageoptions['0'] = get_string('root', 'block_page_tracker');
-    	$pages = course_page::get_all_pages($COURSE->id, 'flat');
-    	foreach($pages as $p){
-    		$pageoptions[$p->id] = format_string($p->nametwo);
-    	}
-		$mform->addElement('select', 'config_startpage', get_string('startpage', 'block_page_tracker'), $pageoptions);
+        $mform->addElement('checkbox', 'config_hidedisabledlinks', get_string('hidedisabledlinks', 'block_page_tracker'));
+        $mform->setDefault('config_hidedisabledlinks', 0);
 
-    	$leveloptions = array();
-    	$leveloptions['100'] = get_string('alllevels', 'block_page_tracker');
-    	for($i = 1; $i <= 3; $i++){
-    		$leveloptions[$i] = $i;
-    	}
-		$mform->addElement('select', 'config_depth', get_string('depth', 'block_page_tracker'), $leveloptions);
+        $pageoptions = array();
+        $pageoptions['0'] = get_string('root', 'block_page_tracker');
+        $pages = course_page::get_all_pages($COURSE->id, 'flat');
+        foreach ($pages as $p) {
+            $pageoptions[$p->id] = format_string($p->nametwo);
+        }
+        $mform->addElement('select', 'config_startpage', get_string('startpage', 'block_page_tracker'), $pageoptions);
 
-		$mform->addElement('advcheckbox', 'config_usemenulabels', get_string('usemenulabels', 'block_page_tracker'), '');
-		$mform->setDefault('config_usemenulabels', 0);
+        $leveloptions = array();
+        $leveloptions['100'] = get_string('alllevels', 'block_page_tracker');
+        for ($i = 1; $i <= 3; $i++) {
+            $leveloptions[$i] = $i;
+        }
+        $mform->addElement('select', 'config_depth', get_string('depth', 'block_page_tracker'), $leveloptions);
 
-		$mform->addElement('advcheckbox', 'config_hideaccessbullets', get_string('hideaccessbullets', 'block_page_tracker'), '');
-		$mform->setDefault('config_hideaccessbullets', 0);
-		
+        $mform->addElement('advcheckbox', 'config_usemenulabels', get_string('usemenulabels', 'block_page_tracker'), '');
+        $mform->setDefault('config_usemenulabels', 0);
+
+        $mform->addElement('advcheckbox', 'config_hideaccessbullets', get_string('hideaccessbullets', 'block_page_tracker'), '');
+        $mform->setDefault('config_hideaccessbullets', 0);
     }
 
     function set_data($defaults, &$files = null) {
 
         if (!$this->block->user_can_edit() && !empty($this->block->config->title)) {
-            // If a title has been set but the user cannot edit it format it nicely
+            // If a title has been set but the user cannot edit it format it nicely.
             $title = $this->block->config->title;
             $defaults->config_title = format_string($title, true, $this->page->context);
             // Remove the title from the config so that parent::set_data doesn't set it.
@@ -79,7 +77,7 @@ class block_page_tracker_edit_form extends block_edit_form {
 
         parent::set_data($defaults);
         if (isset($title)) {
-            // Reset the preserved title
+            // Reset the preserved title.
             $this->block->config->title = $title;
         }
     }
