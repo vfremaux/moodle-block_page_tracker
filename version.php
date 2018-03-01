19e4424ac19f136cafd65c71d94c3e8298d468b3
valery <valery.fremaux@gmail.com>
2018-03-01 16:34:02 +0100

    full resync

--- version.php
+++ version.php
@@ -27,31 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016101105;        // The current plugin version (Date: YYYYMMDDXX).
-<<<<<<< HEAD
-<<<<<<< HEAD
-$plugin->requires  = 2016120500;        // Requires this Moodle version.
-$plugin->component = 'block_page_tracker'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.2 (Build 2016101105)';
-=======
-$plugin->requires  = 2017050500;        // Requires this Moodle version.
-$plugin->component = 'block_page_tracker'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.3 (Build 2016101105)';
->>>>>>> MOODLE_33_STABLE
-=======
 $plugin->requires  = 2017110800;        // Requires this Moodle version.
 $plugin->component = 'block_page_tracker'; // Full name of the plugin (used for diagnostics).
 $plugin->release = '3.4 (Build 2016101105)';
->>>>>>> MOODLE_34_STABLE
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('format_page' => 2016030701);
 
 // Non moodle attributes.
-<<<<<<< HEAD
-<<<<<<< HEAD
-$plugin->codeincrement = '3.2.0001';
-=======
-$plugin->codeincrement = '3.3.0001';
->>>>>>> MOODLE_33_STABLE
-=======
 $plugin->codeincrement = '3.4.0001';
->>>>>>> MOODLE_34_STABLE
