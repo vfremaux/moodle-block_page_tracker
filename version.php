8c1831c51d6522e684eaeaf3f63167ac136da93c
Valery Fremaux <valery.fremaux@gmail.com>
2021-05-05 16:49:37 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -26,22 +26,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016101105;        // The current plugin version (Date: YYYYMMDDXX).
-<<<<<<< HEAD
-$plugin->requires  = 2017110800;        // Requires this Moodle version.
+$plugin->version   = 2019091601;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2020060900;        // Requires this Moodle version.
 $plugin->component = 'block_page_tracker'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.5.0 (Build 2016101105)';
-=======
-$plugin->requires  = 2018112800;        // Requires this Moodle version.
-$plugin->component = 'block_page_tracker'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.6.0 (Build 2016101105)';
->>>>>>> MOODLE_36_STABLE
+$plugin->release = '3.9.0 (Build 2019091601)';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array('format_page' => 2016030701);
+$plugin->dependencies = array('format_page' => 2019091601);
 
 // Non moodle attributes.
-<<<<<<< HEAD
-$plugin->codeincrement = '3.5.0001';
-=======
-$plugin->codeincrement = '3.6.0001';
->>>>>>> MOODLE_36_STABLE
+$plugin->codeincrement = '3.9.0005';
