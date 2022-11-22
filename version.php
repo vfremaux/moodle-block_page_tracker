96c0eda75a4de7e6acb03b7e86590a37eadf4281
Valery Fremaux <valery.fremaux@gmail.com>
2022-11-22 10:56:25 +0100

    %1

--- version.php
+++ version.php
@@ -27,12 +27,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021120200;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020060900;        // Requires this Moodle version.
+$plugin->requires  = 2022041200;        // Requires this Moodle version.
 $plugin->component = 'block_page_tracker'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.9.0 (Build 2021120200)';
+$plugin->release = '4.0.0 (Build 2021120200)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('format_page' => 2019091601);
-$plugin->supported = [39,311];
+$plugin->supported = [40,40];
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.9.0005';
+$plugin->codeincrement = '4.0.0005';
